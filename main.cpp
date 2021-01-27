39a78a6cfb84b8e87ed8c616971dd924dd8a56f0
mtsacchi <mtsacchi@hotmail.com>
2021-01-27 20:19:07 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+	if (argc > 1) return 1;
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "OK!" << std::endl;
+	
+	std::cout << "edit 1: this is for testing the git and github integration!" << std::endl;
+	
+
+	return 0;
+
+}
