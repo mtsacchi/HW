1fb7373786870d1bcd4aa70c32d9930b215fe016
mtsacchi <mtsacchi@hotmail.com>
2021-03-13 18:32:44 -0300

    This are the changes applied to branch editNumber2

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main(int argc, char* argv[]) {
 	std::cout << "OK!" << std::endl;
 	
 	std::cout << "edit 1: this is for testing the git and github integration!" << std::endl;
-	
+	std::cout << "edit 2: branch editNumber2\n";
+	std::cout << "edit 2b\n";
 
 	return 0;
 
---
+++ readme.txt
@@ -0,0 +1,2 @@
+editNumber2
+this is a test file to see if git detects this type of change as well.
